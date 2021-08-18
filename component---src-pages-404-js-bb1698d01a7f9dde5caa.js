e5ac9c04c706cd565aad1b5ff0a0e681db9cb50c
Vinicius Dias <carlosv775@gmail.com>
2021-08-18 12:44:47 -0300

    Updates

---
+++ component---src-pages-404-js-bb1698d01a7f9dde5caa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),l=a(7198),s=a(3751);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bb1698d01a7f9dde5caa.js.map
