ab4be2dc6830141adc0b74fc20087540ed6f2ef2
Vinicius Dias <carlosv775@gmail.com>
2023-03-09 23:21:19 -0300

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 require('../css/app.scss');
+require('lite-youtube-embed/src/lite-yt-embed');
 
-var hljs = require('highlightjs')
+var hljs = require('highlightjs');
 
 hljs.initHighlightingOnLoad();
