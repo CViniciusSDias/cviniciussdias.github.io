585207b65da62b0e2da951a22c2f1f115452155a
Vinicius Dias <carlosv775@gmail.com>
2024-04-09 23:50:28 -0300

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,15 @@
 require('../css/app.scss');
 
-const hljs = require('highlight.js');
+const theme = window?.matchMedia('(prefers-color-scheme: dark)')?.matches ? 'github-dark' : 'github-light';
+document.getElementById('comment-script')
+    .setAttribute('theme', theme);
+
+const hljs = require('highlight.js/lib/core');
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
+hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
+hljs.registerLanguage('http', require('highlight.js/lib/languages/http'));
+hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'));
+hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
+hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'));
 
 hljs.highlightAll();
