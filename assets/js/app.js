aacba0fb66643539119c2661571afb01f2a44113
Vinicius Dias <carlosv775@gmail.com>
2024-04-25 15:58:34 -0300

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,11 @@ hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
 hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'));
 
 hljs.highlightAll();
+
+window.onload = _ => {
+    console.clear();
+
+    console.log("%cDIAS DE DEV", 'color: #550766; font-size: 2rem; font-weight: 900; font-family: Fugaz One,Roboto,sans-serif; border-bottom: .5rem solid #f00696; border-top: .5rem solid #52c196; margin-bottom: 1.25rem; padding: .75rem;');
+    console.log("%cConfere a página \"Sobre Mim\" pra saber como me apoiar para eu continuar criando conteúdo", 'color: #2e353f; font-family: Baloo\ 2, Georgia, Cambria, serif; font-size: 1rem');
+    console.log("%cE se quiser aprender a estilizar mensagens aqui no console, comenta em algum post. ;-)", 'color: #2e353f; font-family: Baloo\ 2, Georgia, Cambria, serif; font-size: 1rem');
+};
