2e2aa337a7c3af218533f3af01c48a02de5ebe1c
Vinicius Dias <carlosv775@gmail.com>
2024-07-15 23:45:37 -0300

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@ hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'));
 hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
 hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'));
 hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'));
+hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
 
 hljs.highlightAll();
 
